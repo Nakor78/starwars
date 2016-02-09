a2f6f41438a6e5ab86f6f97422a9843a2ec39708
Alexandre Brulé <alexandre.brule@devinci.fr>
2016-02-09 15:55:10 +0100

    Features implemented in the HTML but it needs more wow effects

--- index.js
+++ index.js
@@ -34,3 +34,92 @@ var quotes = [{
   'author': '- Darth Vader',
   'quote': 'The Force is strong with this one.'
 }];
+
+var numberOfQuotes = quotes.length;
+var rdm = Math.floor(Math.random() * numberOfQuotes);
+
+var resetRNG = function resetRNG () {  
+  rdm = Math.floor(Math.random() * numberOfQuotes);
+}
+
+var getRandomQuote = function getRandomQuote () {
+  var quoteToReturn = quotes[rdm].quote;
+  return quoteToReturn;  
+};
+
+var getAuthor = function getAuthor () {
+  var authorToReturn = quotes[rdm].author;
+  return authorToReturn;
+};
+
+function userRandomQuote () {
+  var randomQuote = getRandomQuote();
+  document.getElementById("showMeRandomQuote").innerHTML = randomQuote;
+};
+
+function userCharacterName () {
+  var characterName = getAuthor();
+  document.getElementById("showMeCharacterName").innerHTML = characterName;
+};
+
+function quoteYoda () {
+  var rdmYoda = Math.floor(Math.random() * 4);
+
+  if (rdmYoda == 0)
+  {
+    var quoteFromCharacterChosen = quotes[0].quote;
+  }
+  if (rdmYoda == 1)
+  {
+    var quoteFromCharacterChosen = quotes[5].quote;
+  }
+  if (rdmYoda == 2)
+  {
+    var quoteFromCharacterChosen = quotes[7].quote;
+  }
+  if (rdmYoda == 3)
+  {
+    var quoteFromCharacterChosen = quotes[9].quote;
+  }
+  
+  document.getElementById("showMeQuoteFromCharacterChosen").innerHTML = quoteFromCharacterChosen;
+};
+
+function quoteSenatorPalpatine () {
+  var quoteFromCharacterChosen = quotes[1].quote;
+  document.getElementById("showMeQuoteFromCharacterChosen").innerHTML = quoteFromCharacterChosen;
+};
+
+function quoteCountDooku() {
+  var quoteFromCharacterChosen = quotes[2].quote;
+  document.getElementById("showMeQuoteFromCharacterChosen").innerHTML = quoteFromCharacterChosen;
+};
+
+function quoteDarthVader () {
+  var rdmDarthVader = Math.floor(Math.random() * 2);
+
+  if (rdmDarthVader == 0)
+  {
+    var quoteFromCharacterChosen = quotes[3].quote;
+  }
+  if (rdmDarthVader == 1)
+  {
+    var quoteFromCharacterChosen = quotes[10].quote;
+  }
+  document.getElementById("showMeQuoteFromCharacterChosen").innerHTML = quoteFromCharacterChosen;
+};
+
+function quotePrincessLeila () {
+  var quoteFromCharacterChosen = quotes[4].quote;
+  document.getElementById("showMeQuoteFromCharacterChosen").innerHTML = quoteFromCharacterChosen;
+};
+
+function quoteQuiGonJinn () {
+  var quoteFromCharacterChosen = quotes[6].quote;
+  document.getElementById("showMeQuoteFromCharacterChosen").innerHTML = quoteFromCharacterChosen;
+};
+
+function quoteObiWanKenobi () {
+  var quoteFromCharacterChosen = quotes[8].quote;
+  document.getElementById("showMeQuoteFromCharacterChosen").innerHTML = quoteFromCharacterChosen;
+};
